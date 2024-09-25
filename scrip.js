7b4d86ea094bf2e913315e556b118d4cdbd056f4
Fornik2007 <nbaronad2200@alumno.ipn.mx>
2024-09-24 18:01:21 -0600

    creacion del repo

---
+++ scrip.js
@@ -0,0 +1,29 @@
+/*
+CLASE 24/09/24
+NOMBRE: Barona de la Cruz Neftaly
+*/
+//VARIABLES
+/*
+LAS VARIABLES GUARDAN INFO QUE SE PUEDE REUTILIZAR
+*/
+
+let nombre = 'pedro'; //variable que cambia de valor
+const pi = 3.1416 //es una constante que nunca cambia
+var numero = 25 //ya 
+
+//operaciones
+
+let a = 5;
+let b = 2;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+
+//operadores de comparacion
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara tipo)
+
+alert("hola mundo jijijija clase 5IV/") 
