d7459b48dcd1d530cfbdb5064baae632aedeea60
Fornik2007 <nbaronad2200@alumno.ipn.mx>
2024-10-10 16:46:33 -0600

    se agregaron algunos comentarios para mejorar compresion

--- clase_26-09-24/script.js
+++ clase_26-09-24/script.js
@@ -1,16 +1,20 @@
+//esta funcion concatena los valores de los botones cuando se hace click
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
+//lo que hace esta funcion no es como tal borrar sino qur sobre escribe sobre los datos anteriores, pero como esta en blanco lo sobrescribe en blanco
 function clearDisplay() {
     document.getElementById('display').value = "";
 }
 
+//discrimina el ultimo valor para borrar el ultimo numero
 function deleteLast() {
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
 
+//esta funcion es la que realiza las operaciones y detecta algunos errores
 function calculateResult() {
     try {
         let result = eval(document.getElementById('display').value);
