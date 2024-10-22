3eba125927b8400123c0e08435da4d62b4eb4070
Fornik2007 <nbaronad2200@alumno.ipn.mx>
2024-10-22 17:44:40 -0600

    funciones flecha

---
+++ clase_22-10-24/script.js
@@ -0,0 +1,27 @@
+//01 funcion normal
+function hola (a, b ) {
+    return a + b;
+}
+
+console.log(hola(3,5));
+
+//02 funcion flecha
+const suma = (a,b) => {
+    return a + b;
+}
+
+console.log(suma(8,6));
+
+//03 simplificar la funcion flecha
+const suma2 = (a,b) => a + b;
+
+console.log(suma2(4,5));
+
+//04 solo un parametro
+const cuadrado = x => x*x;
+console.log(cuadrado(50))
+
+
+
+
+
